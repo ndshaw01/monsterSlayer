68ade36de866306555ca37381704b57737e09b93
Nathan Shaw <natedogiam@gmail.com>
2020-04-28 18:49:44 +0000

    added first bit of Vue functionality, changing the gamestate

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 new Vue({
+	el:"#app",
+	data:{
+		gameStart: true,
+		playerHealth: 100,
+		monsterHealth: 100,
+		log: []
+	},
+	methods: {
+		startGame: function(){
+			this.gameStart = !this.gameStart;
+		}
+	}
 
-	
 });
